b170b04e43c5101b599e2ba23390c25acba1c5d6
JaberQayad <jaberqayad@gmail.com>
2026-01-10 02:32:18 +0100

    refactor: logger to output to console only

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,32 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
-// Create logs directory if it doesn't exist
-const logsDir = path.join(__dirname, '../../logs');
-if (!fs.existsSync(logsDir)) {
-    fs.mkdirSync(logsDir, { recursive: true });
-}
-
-// Generate log filename with timestamp
-const timestamp = new Date().toISOString().replace(/[:.]/g, '-').split('T')[0];
-const timeString = new Date().toISOString().split('T')[1].split('.')[0].replace(/:/g, '-');
-const logFileName = `${timestamp}_${timeString}.log`;
-const logFilePath = path.join(logsDir, logFileName);
-
 /**
- * Logger utility for logging to both console and file
+ * Logger utility for logging to console
  */
 class Logger {
-    constructor(filePath) {
-        this.filePath = filePath;
-        this.writeStream = fs.createWriteStream(filePath, { flags: 'a' });
-        
-        // Handle stream errors
-        this.writeStream.on('error', (err) => {
-            console.error('Error writing to log file:', err);
-        });
-    }
-
     /**
      * Format log message with timestamp
      */
@@ -37,19 +12,11 @@ class Logger {
     }
 
     /**
-     * Write to file
-     */
-    writeToFile(message) {
-        this.writeStream.write(message + '\n');
-    }
-
-    /**
      * Log info level
      */
     info(message, data) {
         const formatted = this.formatMessage('INFO', message, data);
         console.log(formatted);
-        this.writeToFile(formatted);
     }
 
     /**
@@ -61,7 +28,6 @@ class Logger {
             : error;
         const formatted = this.formatMessage('ERROR', message, errorDetails);
         console.error(formatted);
-        this.writeToFile(formatted);
     }
 
     /**
@@ -70,7 +36,6 @@ class Logger {
     warn(message, data) {
         const formatted = this.formatMessage('WARN', message, data);
         console.warn(formatted);
-        this.writeToFile(formatted);
     }
 
     /**
@@ -79,30 +44,10 @@ class Logger {
     debug(message, data) {
         const formatted = this.formatMessage('DEBUG', message, data);
         console.log(formatted);
-        this.writeToFile(formatted);
-    }
-
-    /**
-     * Get current log file path
-     */
-    getLogPath() {
-        return this.filePath;
-    }
-
-    /**
-     * Close the stream
-     */
-    close() {
-        this.writeStream.end();
     }
 }
 
 // Create and export logger instance
-const logger = new Logger(logFilePath);
-
-// Handle process exit to close stream gracefully
-process.on('exit', () => {
-    logger.close();
-});
+const logger = new Logger();
 
 module.exports = logger;
